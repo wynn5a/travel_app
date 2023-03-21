3c918a80b26971ce37c81e27b571f010c252e84a
wynn5a <winminy@163.com>
2023-03-21 16:58:14 +0800

    New: navigation bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:travel_app/components/destination_card.dart';
 import 'package:travel_app/components/destination_list.dart';
 import 'package:travel_app/components/header.dart';
 import 'package:travel_app/components/label_section.dart';
@@ -23,14 +22,33 @@ class HomePage extends StatelessWidget {
               const SearchBar(),
               SizedBox(height: medium),
               LabelSection(label: 'Recommended', labelStyle: heading1),
-              SizedBox(height: medium),
+              SizedBox(height: small),
               const DestinationListView(),
               SizedBox(height: medium),
               LabelSection(label: 'Top Destination', labelStyle: heading2),
-              SizedBox(height: medium),
+              SizedBox(height: small),
               const TopDestinationList()
             ],
           ),
+        ),
+      ),
+      bottomNavigationBar: SizedBox(
+        height: 90,
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: accent,
+          unselectedItemColor: icon,
+          elevation: 0,
+          backgroundColor: white,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.explore_outlined), label: ''),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.bookmark_outline_outlined), label: ''),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_outlined), label: ''),
+          ],
         ),
       ),
     );
